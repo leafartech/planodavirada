65f453014fca42ccf497c69431a6f50892042c52
leafartech <119262053+leafartech@users.noreply.github.com>
2024-06-12 10:23:30 -0300

    gtm added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <body className={`${inter.className} ${playfair_display.className}`}>{children}</body>
+      <head>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-KK8LV559')
+        `}} />
+      </head>
+      <body className={`${inter.className} ${playfair_display.className}`}>
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KK8LV559"
+          height="0" width="0"></iframe></noscript>
+        {children}
+      </body>
     </html>
   );
 }
